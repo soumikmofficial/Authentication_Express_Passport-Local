2be88404095229df1a895756bec020d9f718558e
soumikm.official@gmail.com <soumikmofficial@gmail.com>
2021-11-02 19:06:11 +0530

    registration alerts and validation initalized not finalized

--- controllers/auth.js
+++ controllers/auth.js
@@ -10,13 +10,40 @@ const loginPage = (req, res) => {
 const registerPage = (req, res) => {
   res.render("register", { title: "Register" });
 };
+const dashboard = (req, res) => {
+  res.render("dashboard", { title: "Dashboard" });
+};
 
 // **the auth functons**
 
 const register = async (req, res) => {
+  const { email, password1, password2 } = req.body;
+  let errors = [];
+
+  if (!email || !password1 || !password2) {
+    errors.push({ msg: "All the fields are required" });
+  }
+
+  if (password1 !== password2) {
+    errors.push({ msg: "Passwords do not match" });
+  }
+  if (password1.length < 6 || password2.length < 6) {
+    errors.push({ msg: "Password must be 6 characters or longer" });
+  }
+
+  if (errors.length > 1) {
+    return res.render("register", {
+      errors,
+      email,
+      password1,
+      password2,
+      title: register,
+    });
+  }
   console.log(req.body);
-  res.send("working");
+  res.redirect("/dashboard");
 };
+
 const login = async (req, res) => {
   console.log(req.body);
   res.send("login");
